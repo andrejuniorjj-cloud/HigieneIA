ec37dbd87213de402a43fe67523244ad0bab6e79
andrejuniorjj-cloud <andrejuniorjj@gmail.com>
2025-08-28 00:33:59 -0300

    Update main.tsx

--- client/client/src/main.tsx
+++ client/client/src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css"; // importa o tailwind
 
 const App = () => {
-  return <h1>HigieneIA funcionando!</h1>;
+  return <h1 className="text-3xl font-bold text-blue-600">HigieneIA funcionando!</h1>;
 };
 
 createRoot(document.getElementById("root")!).render(<App />);
