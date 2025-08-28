40e1f0958a2c7bbb982ccad3794aa0955fa1fa05
andrejuniorjj-cloud <andrejuniorjj@gmail.com>
2025-08-27 23:52:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'client'), // onde está seu index.html
+  root: path.resolve(__dirname, 'client'), // aponta para a pasta onde está index.html
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: path.resolve(__dirname, 'dist'), // diretório de saída final
+    emptyOutDir: true, // limpa a pasta antes do build
   },
 });
