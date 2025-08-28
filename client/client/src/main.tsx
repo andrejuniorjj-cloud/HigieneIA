1d1113c1b76a11c11670a6d06a524417e3bb8cbc
andrejuniorjj-cloud <andrejuniorjj@gmail.com>
2025-08-28 00:01:44 -0300

    Create main.tsx

---
+++ client/client/src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <h1>HigieneIA funcionando!</h1>;
+};
+
+createRoot(document.getElementById("root")!).render(<App />);
