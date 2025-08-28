899f6f57eb28321147b61e105179b916fce6750e
andrejuniorjj-cloud <andrejuniorjj@gmail.com>
2025-08-27 23:44:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,3 +11,14 @@ export default defineConfig({
     }
   }
 })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  root: path.resolve(__dirname, 'client'),
+  plugins: [react()],
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+  },
+});
